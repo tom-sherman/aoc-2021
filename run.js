55b86c62d60ba2ab56c9055581a7609f85d0b5f4
Tom Sherman <the.tomsherman@gmail.com>
2021-12-05 13:12:55 +0000

    Ensure errors are logged

--- run.js
+++ run.js
@@ -15,8 +15,12 @@ const fs = require('fs/promises');
   console.log('Part: ', puzzlePart);
   console.log('Reading input from: ', inputPath);
   const input = (await fs.readFile(inputPath)).toString();
-  const output = await puzzle.solve(input, puzzlePart);
-
-  console.log('\nGot output:\n');
-  console.log(output);
+  try {
+    const output = await puzzle.solve(input, puzzlePart);
+    console.log('\nGot output:\n');
+    console.log(output);
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
 })();
